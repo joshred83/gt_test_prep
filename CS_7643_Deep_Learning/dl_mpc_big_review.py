14e03fd186c8da78b643c52884f9afcce148590c
Ginger de Ridder <ginger_de_ridder@hotmail.com>
2024-10-02 22:28:05 +0800

    Quick commit

--- CS_7643_Deep_Learning/dl_mpc_big_review.py
+++ CS_7643_Deep_Learning/dl_mpc_big_review.py
@@ -20,7 +20,7 @@ def big_review():
 
     # Select Quiz
     st.title("Select a Quiz")
-    quiz_choice = st.radio("Choose a quiz", options=["Quiz 1", "Quiz 2"])
+    quiz_choice = st.radio("Choose a quiz", options=["Quiz 1", "Quiz 2","Quiz 3" ])
 
     # Get lessons for the selected quiz
     selected_quiz_lessons = quiz_mapping[quiz_choice]
